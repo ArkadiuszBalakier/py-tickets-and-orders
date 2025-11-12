552a6a06045e5eae3ab74937ba3b570b330d2b47
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-11-12 13:00:26 +0100

    add index on title in Movie

--- db/models.py
+++ db/models.py
@@ -17,7 +17,7 @@ class Actor(models.Model):
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, db_index=True)
     description = models.TextField()
     actors = models.ManyToManyField(to=Actor, related_name="movies")
     genres = models.ManyToManyField(to=Genre, related_name="movies")
@@ -50,3 +50,4 @@ class MovieSession(models.Model):
 
     def __str__(self) -> str:
         return f"{self.movie.title} {str(self.show_time)}"
+

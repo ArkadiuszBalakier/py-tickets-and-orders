22d26e79f57f8e94718dd309b11512766dbf8402
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-11-12 14:22:15 +0100

    services - adding create_user

---
+++ services/user.py
@@ -0,0 +1,25 @@
+from db.models import User
+
+
+def create_user(
+        username: str,
+        password: str,
+        email: str = None,
+        first_name: str = None,
+        last_name: str = None,
+) -> None:
+
+        if not username or not password:
+            raise ValueError("Username and password are required")
+
+        user = User.objects.create_user(
+            username=username,
+            password=password,
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+        )
+
+        user.set_password(password)
+        user.save()
+
